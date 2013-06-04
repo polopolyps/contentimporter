7e3b61fec1977c5107b8cd86edf21f61cafa778b
unknown <lhuang@atex.com>
2013-06-04 14:03:33 +0800

    DEVFORUM-110: apply changes to ContentImporter

--- src/main/java/com/polopoly/ps/contentimporter/hotdeploy/file/FileDeploymentFile.java
+++ src/main/java/com/polopoly/ps/contentimporter/hotdeploy/file/FileDeploymentFile.java
@@ -11,40 +11,31 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.zip.CRC32;
 
-public class FileDeploymentFile extends AbstractDeploymentObject
-    implements DeploymentFile, TemplateDefinitionAware
-{
+public class FileDeploymentFile extends AbstractDeploymentObject implements DeploymentFile, TemplateDefinitionAware {
     private static final int BUF_SIZE = 1024 * 64;
     private static final Logger logger = Logger.getLogger(FileDeploymentFile.class.getName());
 
     protected File file;
     private boolean isTemplateDefinitionFile;
 
-    public FileDeploymentFile(final File file)
-    {
+    public FileDeploymentFile(final File file) {
         this.file = file;
     }
 
-    public InputStream getInputStream()
-        throws FileNotFoundException
-    {
+    public InputStream getInputStream() throws FileNotFoundException {
         return new FileInputStream(file);
     }
 
-    public String getName()
-    {
+    public String getName() {
         return file.getAbsolutePath();
     }
 
-    public URL getBaseUrl()
-        throws MalformedURLException
-    {
+    public URL getBaseUrl() throws MalformedURLException {
         File parentFile = getParentFile();
         return parentFile.toURI().toURL();
     }
 
-    File getParentFile()
-    {
+    File getParentFile() {
         File result = file.getParentFile();
 
         if (result == null) {
@@ -54,35 +45,30 @@ public class FileDeploymentFile extends AbstractDeploymentObject
         return result;
     }
 
-    public boolean isTemplateDefinitionFile()
-    {
+    public boolean isTemplateDefinitionFile() {
         return isTemplateDefinitionFile;
     }
 
-    public void setIsTemplateDefinitionFile(final boolean isTemplateDefinitionFile)
-    {
+    public void setIsTemplateDefinitionFile(final boolean isTemplateDefinitionFile) {
         this.isTemplateDefinitionFile = isTemplateDefinitionFile;
     }
 
-    public String getDirectory()
-    {
+    public String getDirectory() {
         return getParentFile().getAbsolutePath();
     }
 
-    public long getQuickChecksum()
-    {
+    public long getQuickChecksum() {
         return file.lastModified();
     }
 
-    public long getSlowChecksum()
-    {
+    public long getSlowChecksum() {
         CRC32 checksum = new CRC32();
 
         byte[] buffer = new byte[BUF_SIZE];
 
         try {
             InputStream inputStream = getInputStream();
-            
+
             int readBytes;
 
             do {
@@ -92,27 +78,27 @@ public class FileDeploymentFile extends AbstractDeploymentObject
                     checksum.update(buffer, 0, readBytes);
                 }
             }
-            
+
             while (readBytes == BUF_SIZE);
 
             inputStream.close();
             return checksum.getValue();
         } catch (FileNotFoundException e) {
             logger.log(Level.WARNING, e.getMessage(), e);
+            System.err.println(e.getMessage());
             return 0;
         } catch (IOException e) {
             logger.log(Level.WARNING, e.getMessage(), e);
+            System.err.println(e.getMessage());
             return 0;
         }
     }
 
-    public File getFile()
-    {
+    public File getFile() {
         return file;
     }
 
-    public boolean imports(final DeploymentObject object)
-    {
+    public boolean imports(final DeploymentObject object) {
         return object.equals(this);
     }
 }
